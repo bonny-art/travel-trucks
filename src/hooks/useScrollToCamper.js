50cf0b85bd875ff3fc330cbf425c474ddc99b2a2
bonny-art <svitlana.otenko@gmail.com>
2025-06-08 06:38:42 +0300

    Try to fix scroll ro card

--- src/hooks/useScrollToCamper.js
+++ src/hooks/useScrollToCamper.js
@@ -8,14 +8,15 @@ const useScrollToCamper = () => {
   useEffect(() => {
     const scrollToCamperId = location.state?.scrollToId;
     console.log("🚀 ~ scrollToCamperId:", scrollToCamperId);
+    console.log("🚀 ~ hasScrolled.current:", hasScrolled.current);
     if (scrollToCamperId && !hasScrolled.current) {
-      hasScrolled.current = true;
       setTimeout(() => {
         const el = document.getElementById(`camper-${scrollToCamperId}`);
         if (el) {
           console.log("🚀 ~ el:", el);
           el.scrollIntoView({ behavior: "smooth", block: "center" });
         }
+        hasScrolled.current = true;
       }, 300);
     }
   }, [location.state?.scrollToId]);
